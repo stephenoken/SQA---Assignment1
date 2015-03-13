608ce562db1d08553b0eb339e52674e2b6536db5
Stephen O'Kennedy <stephenoken@gmail.com>
2015-03-13 19:32:21 +0000

    Completed the first section and have part 1 section 2 Complete
    Also made changes to the javascript.

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,25 @@
 $(document).ready(function () {
-    var overview = $('.overview');
-    var origOffsetY = overview.offset().top;
+    var overview1 = $('#overview-1');
+	var overview2 = $('#overview-2');
+    var overviewOriginOffSet1Start = overview1.offset().top;
+	var overviewOriginOffSet2Start = overview2.offset().top -150;
+
 
     function scroll() {
-        if ($(window).scrollTop() >= origOffsetY) {
-            $('.overview').addClass('sticky');
-        } else {
-            $('.overview').removeClass('sticky');
+        if ($(window).scrollTop() >= overviewOriginOffSet1Start) {
+            $('#overview-1').addClass('sticky');
+			$('#main-body-1').removeClass('rebalance-main-content');
+        }else {
+            $('#overview-1').removeClass('sticky');
+			$('#main-body-1').addClass('rebalance-main-content');
         }
-
+		if ($(window).scrollTop()>=overviewOriginOffSet2Start) {
+			 $('#overview-2').addClass('sticky');
+			 $('#overview-1').addClass('disappear');
+		} else {
+			 //$('#overview-2').removeClass('sticky');
+			 $('#overview-1').removeClass('disappear');
+		}
 
     }
 
